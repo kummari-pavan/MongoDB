6dcc501cf09749aa4a3bcfbd68e4355af24ca347
kummari-pavan <kpavan892002@gmail.com>
2024-10-08 11:55:37 +0530

    [PAVAN]ADD: adding Mongo_Js_Files for VS Code Practice

---
+++ Mongo_Js_Files/insert.mongodb.js
@@ -0,0 +1,7 @@
+use ('PServer')
+
+db.Trainee.insertOne({name:"Jhon",ID:"BLZ186",Company:"Wipro"})
+
+db.Trinee.insertMany([{name:"ABC",ID:"BLZ181",Company:"Wipro"},{name:"DEF",ID:"BLZ182",Company:"Wipro"},
+    {name:"HIJ",ID:"BLZ183",Company:"Wipro"},{name:"KLM",ID:"BLZ184",Company:"Wipro"}])
+

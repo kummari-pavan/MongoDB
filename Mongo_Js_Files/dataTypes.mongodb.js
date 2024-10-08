d8abe7b0f7d0e63d4ecb7c76bc43d7cabab84229
kummari-pavan <kpavan892002@gmail.com>
2024-10-08 12:27:38 +0530

    [PAVAN]ADD: need Small Changes

---
+++ Mongo_Js_Files/dataTypes.mongodb.js
@@ -0,0 +1,13 @@
+use ("PServer")
+
+db.Trainee.insertOne({
+    name: "Pavan",
+    age: 22,
+    isEmployed: true,
+    Backlogs:null,
+    skills: ["JavaScript", "MongoDB"],
+    joinedDate: new Date(),
+    Ratindg:{JavaScript:5,MongoDB:5}
+}) 
+
+db
